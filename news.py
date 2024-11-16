8dd2a3c6f467836af5db8fea4383f6b30f77b983
ryabovd <ryabovd@outlook.com>
2024-11-16 22:05:39 +0700

    First work :)

---
+++ news.py
@@ -0,0 +1,173 @@
+from urllib.request import urlopen
+from urllib.error import HTTPError
+from bs4 import BeautifulSoup
+import datetime
+import json
+import csv
+from send_email import send_notification
+"""
+import requests
+import certifi
+
+from send_email import send_notification
+
+
+import sys
+import re
+import time
+"""
+
+"""def getTitle(url):
+    try:
+        html = urlopen(url)
+    except HTTPError as e:
+        return None
+    try:
+        bs = BeautifulSoup(html.read(), 'html.parser')
+        title = bs.body.h1
+    except AttributeError as e:
+        return None
+    return title"""
+
+
+def getContainer(url):
+    try:
+        html = urlopen(url)
+    except HTTPError as e:
+        return None
+    try:
+        bs = BeautifulSoup(html.read(), 'html.parser')
+        container = bs.div(attrs= {'class': 'info-box'}) 
+    except AttributeError as e:
+        return None
+    return container
+
+
+def getData(new):
+    data = new.find('div', {'class': 'info-box__data'}).get_text()
+    return data
+
+
+def getNewText(new):
+    newText = new.find('a').get_text()
+    return newText
+
+
+def getLink(new):
+    newLink = new.find('a', href = True)['href']
+    return newLink
+
+
+def get_current_date(soup):
+    current_date = soup[0].find('div', {'class': 'info-box__data'}).get_text()
+    return current_date
+
+
+def convert_date(date):
+    date_list = date.split('.')
+    year, month, day = int(date_list[2]), int(date_list[1]), int(date_list[0])
+    return datetime.date(year, month, day)
+
+
+def dates_diff(last_date, current_date):
+    return convert_date(current_date) > convert_date(last_date)
+
+
+def get_settings():
+    with open('news_settings.json', 'r', encoding='utf-8') as file:
+        settings = json.load(file)
+    return settings
+
+
+def save_news(news, CSV):
+    with open(CSV, 'w', encoding='utf-8')  as csvfile:
+        writer = csv.writer(csvfile, delimiter=';')
+        writer.writerow(['Дата', 'Заголовок', 'Ссылка', 'Текст'])
+        for news_dict in news:
+            writer.writerow( [news_dict['news_date'], news_dict['news_title'], news_dict['news_link']] )
+
+
+def get_news_from_contenteiner(container, last_date):
+    news_list_dict = []
+    for new in container:
+        data = getData(new)
+        if convert_date(last_date) < convert_date(data):
+            new_text = getNewText(new)
+            new_link = getLink(new)
+            news_list_dict.append(
+                {
+                    'news_date': data,
+                    'news_title': new_text,
+                    'news_link': new_link
+                }
+            )
+    return news_list_dict
+
+
+def text_for_send(news):
+    '''That func prepared text of message and add footer with add_footer func'''
+    text = ''
+    for news_dict in news:
+        text += news_dict['news_date'] + '\n' + news_dict['news_title'] + '\n' + news_dict['news_link'] + '\n' + '\n'
+    text += add_footer()
+    return text
+
+
+def add_footer():
+    '''In that func contains text of message's footer.'''
+    footer = '\nОТКАЗАТЬСЯ от получения рассылок ▼ \n\nmailto:secretary@you-right.info?subject=UNSUBSCRIBE&body=Не%20присылайте%20больше%20писем'
+    return footer
+
+
+def write_new_settings_json(settings):
+    with open('main_news_settings.json', 'w', encoding='utf-8') as file:
+        json.dump(settings, file, indent=4, ensure_ascii=False)
+
+
+def get_adress_list(settings):
+    adresses = settings['adress_list'].items()
+    adress_list = []
+    for _, v in adresses:
+        adress_list.append(v)
+    return adress_list
+
+
+def main():
+    CSV = 'news_arbitr.ru.csv'
+    container = getContainer('https://khakasia.arbitr.ru/news-isfb')
+    if container == None:
+        print('Container could not be found')
+    else:
+        settings = get_settings()
+        last_date = settings['last_date']
+        current_date = get_current_date(container)
+        #print(current_date)
+        if dates_diff(last_date, current_date) == True:
+            print('Есть НОВОСТИ')
+            news = get_news_from_contenteiner(container, last_date)
+            print('\nНовости получены')
+            save_news(news, CSV)
+            print('Новости сохранены')
+            text = text_for_send(news)
+            subject = 'court' + ' | Новости на'
+            adress_list = get_adress_list(settings)
+            send_notification(text, subject, adress_list)
+            new_last_date = news[0]['news_date']
+            settings['last_date'] = new_last_date
+            write_new_settings_json(settings)
+        else:
+            print('Новости ОТСУТСТВУЮТ\n')
+    print('Работа скрипта ЗАВЕРШЕНА\n')
+
+# Написать вывод в консоль время выполнения скрипта
+# Исправить ошибку проверки SSL
+# Написать функцию для работы с аргументами строки (с какой даты, выводить в консоль, отправлять по почте)
+# +Исправить функцию записи json, чтобы записывать красивые файлы
+# Написать функцию проверки по расписанию ???
+# +Переписать send_email.py, чтобы она брала список адресов из вне. Придумать где и как хранить список адресов или список адресов и фамилий
+# +Сделать функцию проверки даты прошлой первой новости
+# +Написать функцию записи новой даты новостей
+# +Переписать скрипт на __main__
+# +Написать функцию проверки даты последней новости, полученной при предыдущей проверке, и отбирающей только новые новости
+# +Написать функцию отправки новостей по электронной почте
+"""
---
+++ send_email.py
@@ -0,0 +1,29 @@
+import datetime
+import smtplib
+import json
+import time
+
+
+def send_notification(text, subject, adress_list):
+    email = adress_list
+    with open('settings.json', 'r', encoding='utf-8') as file:
+        settings = json.load(file)
+        sender = settings["sender"]
+        sender_password = settings["sender_password"]
+    mail_lib = smtplib.SMTP_SSL('smtp.yandex.ru', 465)
+    mail_lib.login(sender, sender_password)
+    for to_item in email:
+        msg = 'From: %s\r\nTo: %s\r\nContent-Type: text/plain; charset="utf-8"\r\nSubject: %s\r\n\r\n' % (
+            sender, to_item, '{} {}'.format(subject, date_today()))
+        msg += text
+        mail_lib.sendmail(sender, to_item, msg.encode('utf8'))
+        print('Отправлено письмо на адрес {}'.format(to_item))
+        time.sleep(2)
+    print('\n')
+    mail_lib.quit()
+
+
+def date_today():
+    '''Func that returned today date'''
+    today = datetime.date.today()
+    return today
